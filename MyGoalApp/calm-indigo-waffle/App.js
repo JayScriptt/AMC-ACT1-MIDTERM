181a8aa0d66a0fc5bfd878a4a1f90dcf313e0f8d
Cornelio A. Gatbonton Jr <139235141+JayScriptt@users.noreply.github.com>
2025-03-13 17:00:03 +0800

    Update App.js

--- MyGoalApp/calm-indigo-waffle/App.js
+++ MyGoalApp/calm-indigo-waffle/App.js
@@ -1,134 +1,126 @@
-import React, {useState} from 'react'
+import React, {useState} from 'react';
 import { View, TextInput, Button, FlatList, Text, TouchableOpacity, StyleSheet} from 'react-native';
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState('');
+  const [courseGoal, setCourseGoal] = useState([]);
+  const [editingIndex, setEditingIndex] = useState(null);
 
-const [enteredGoalText, setEnteredGoalText] = useState('');
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoalText(enteredText);
+  };
 
-const [courseGoal, setCourseGoal] = useState([]);
-
-const [editingIndex, setEditingIndex] = useState(null);
-
-const goalInputHandler = (enteredText) => {
-  setEnteredGoalText(enteredText)
-};
-
-const addGoalHandler = () => {
+  const addGoalHandler = () => {
     if (enteredGoalText.trim() === '') return;
 
     if(editingIndex !== null){
       const updatedGoals = [...courseGoal];
       updatedGoals[editingIndex] = { text: enteredGoalText, id: updatedGoals[editingIndex].id};
-      setCourseGoal(updatedGoals)
+      setCourseGoal(updatedGoals);
       setEditingIndex(null);
     } else {
       setCourseGoal((currentCouseGoal) => [
         ...currentCouseGoal,
         { text: enteredGoalText, id: Math.random().toString() }
-      ])
+      ]);
     }
-  
-  setEnteredGoalText('');
-};
+    setEnteredGoalText('');
+  };
 
-const deleteGoal = (index) => {
-  const updatedGoals = [...courseGoal];
-  updatedGoals.splice(index, 1);
-  setCourseGoal(updatedGoals);
-  if (editingIndex === index) setEditingIndex(null);
-};
+  const deleteGoal = (index) => {
+    const updatedGoals = [...courseGoal];
+    updatedGoals.splice(index, 1);
+    setCourseGoal(updatedGoals);
+    if (editingIndex === index) setEditingIndex(null);
+  };
 
-const startEditing = (index) => {
-  setEnteredGoalText(courseGoal[index].text);
-  setEditingIndex(index);
-}
+  const startEditing = (index) => {
+    setEnteredGoalText(courseGoal[index].text);
+    setEditingIndex(index);
+  };
 
-  return(
-  <View style={styles.container}>
+  return (
+    <View style={styles.container}>
       <View>
-      <Text
-        style={styles.header}>
-        My Goal
-        </Text>
+        <Text style={styles.header}>My Goal</Text>
         <TextInput
           style={styles.input}
-          placeholder = "Enter Here"
+          placeholder="Enter Here"
           onChangeText={goalInputHandler}
           value={enteredGoalText}
-   
         />
-            <Button
-              title= {editingIndex !== null? 'Edit Goal' : 'Add Goal'} onPress={addGoalHandler} /> 
+        <Button title={editingIndex !== null? 'Edit Goal' : 'Add Goal'} onPress={addGoalHandler} />
       </View>
-      
-  <FlatList 
-    style = {styles.flatlist}
-    data = {courseGoal}
-    renderItem={({item, index}) => (
-    <View style={styles.goalItem}>
-      <Text>{item.text}</Text>
-      <TouchableOpacity
-      onPress={() => deleteGoal(index)}>
-      <Text
-      style = {styles.delete}
-      >X</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => startEditing(index)}>
-      <Text
-          style = {styles.edit}
-          >Edit</Text>
-      </TouchableOpacity>
-    </View>
-  )}
-  keyExtractor={(item) => item.id}
-  numColumns={2}
-  />
-  </View>
 
+      <FlatList
+        style={styles.flatlist}
+        data={courseGoal}
+        renderItem={({item, index}) => (
+          <View style={styles.goalItem}>
+            <TouchableOpacity onPress={() => startEditing(index)} style={styles.button}>
+              <Text style={styles.edit}>Edit</Text>
+            </TouchableOpacity>
+            <Text style={styles.text}>{item.text}</Text>
+            <TouchableOpacity onPress={() => deleteGoal(index)} style={styles.button}>
+              <Text style={styles.delete}>X</Text>
+            </TouchableOpacity>
+          </View>
+        )}
+        keyExtractor={(item) => item.id}
+        numColumns={1}
+      />
+    </View>
   );
 }
+
 const styles = StyleSheet.create({
-container: {
-  marginTop: '20%',
-    borderWidth: 1
-},
-header:{
-  fontWeight: 'bold',
-  textAlign: 'center',
-  fontSize: 20,
-  padding: 10
-},
-input:{
+  container: {
+    marginTop: '20%',
     borderWidth: 1,
-    marginBottom: 5, 
-    marginTop: 5
-},
-flatlist:{
+  },
+  header: {
+    fontWeight: 'bold',
+    textAlign: 'center',
+    fontSize: 20,
+    padding: 10,
+  },
+  input: {
     borderWidth: 1,
-},
-goalItem: {
-    flex: 1,
+    marginBottom: 5,
+    marginTop: 5,
+  },
+  flatlist: {
+    borderWidth: 1,
+  },
+  goalItem: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
     margin: 5,
     padding: 10,
     borderWidth: 1,
-    alignItems: 'center'
-},
-delete:{
+  },
+  text: {
+    flex: 1,
+    textAlign: 'center',
+  },
+  delete: {
     borderWidth: 1,
     backgroundColor: 'red',
     color: 'white',
     textAlign: 'center',
-    marginTop: 5,
     padding: 5,
-    width: '100%'
-},
-edit:{
+    width: 50,
+  },
+  edit: {
     borderWidth: 1,
     backgroundColor: 'green',
     color: 'white',
     textAlign: 'center',
-    marginTop: 5,
     padding: 5,
-    width: '100%'
-}
+    width: 50,
+  },
+  button: {
+    width: '20%',
+  },
 });
